1f4bcdfc04a3b8e73ddac222d591c25deaf27f02
Joe Warner <joewarnerdev@gmail.com>
2020-01-07 21:22:57 -0600

    added more product features

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieSession({
-    keys: ['asdfwerojklnm']
-    })
+app.use(
+  cookieSession({
+    keys: ['lkasld235j']
+  })
 );
 app.use(authRouter);
 app.use(productsRouter);
 
 app.listen(3000, () => {
-    console.log('Listening');
+  console.log('Listening');
 });
